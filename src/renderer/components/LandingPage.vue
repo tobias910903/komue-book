c3b86d87899f94467d741c1aadda7c983e084fd4
lihuyong <admin@lihuyong.com>
2020-03-07 01:26:35 +0800

    创建应用

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="wrapper">
+    首页
+    <system-information></system-information>
+  </div>
+</template>
+
+<script>
+  import SystemInformation from './LandingPage/SystemInformation'
+
+  export default {
+    name: 'landing-page',
+    components: { SystemInformation },
+    methods: {
+
+    }
+  }
+</script>
+
+<style>
+
+</style>
