9fd2b68e453a2739930e818070fe771e8564d3da
lihuyong <admin@lihuyong.com>
2020-03-16 22:04:51 +0800

    修改提示

--- src/renderer/components/aboutInfo.vue
+++ src/renderer/components/aboutInfo.vue
@@ -1,33 +1,24 @@
 <template>
     <div class="about-info">
-        <el-card class="box-card">
-            <div slot="header" class="clearfix">
-                <span>使用说明</span>
-            </div>
-            <p>保存：Ctrl+S</p>
-        </el-card>
-
-        <el-card class="box-card">
-            <div slot="header" class="clearfix">
-                <span>更新日志</span>
-            </div>
-
-            <el-timeline>
-                <el-timeline-item timestamp="2020/03/16" placement="top">
-                    <el-card>
-                        <h4>v1.0.1</h4>
-                        <ol>
-                            <li>完成打包</li>
-                        </ol>
-                    </el-card>
-                </el-timeline-item>
-                <el-timeline-item timestamp="2020/03/07" placement="top">
-                    <el-card>
-                        <p>搭环境 建项目</p>
-                    </el-card>
-                </el-timeline-item>
-            </el-timeline>
-        </el-card>
+        <el-alert title="使用说明" type="info" :closable="false"></el-alert>
+        <p>保存：Ctrl+S</p>
+        <br/>
+        <el-alert title="更新日志" type="info" :closable="false"></el-alert>
+        <el-timeline>
+            <el-timeline-item timestamp="2020/03/16" placement="top">
+                <el-card>
+                    <h4>v1.0.1</h4>
+                    <ol>
+                        <li>完成打包</li>
+                    </ol>
+                </el-card>
+            </el-timeline-item>
+            <el-timeline-item timestamp="2020/03/07" placement="top">
+                <el-card>
+                    <p>搭环境 建项目</p>
+                </el-card>
+            </el-timeline-item>
+        </el-timeline>
     </div>
 </template>
 
@@ -47,31 +38,38 @@
         height: 100%;
         background: url("../assets/images/icon.png") center;
 
-        .box-card {
+        /deep/ .box-card {
             & + .box-card {
                 margin-top: 20px;
             }
 
-            /deep/ .el-card__header {
+            .el-card__header {
                 font-size: 18px;
             }
+        }
 
-            h4 {
-                margin: 0 0 10px;
-                font-size: 16px;
-                font-weight: 500;
-            }
 
-            ol {
-                padding-left: 15px;
-                margin: 0;
-                color: #999999;
-            }
+        /deep/ .el-alert{
+            margin-bottom: 15px;
+        }
 
-            p {
-                margin: 0;
-                color: #999999;
-            }
+        h4 {
+            margin: 0 0 5px;
+            font-size: 16px;
+            font-weight: 500;
+        }
+
+        ol {
+            padding-left: 15px;
+            margin: 0;
+            color: #999999;
+            font-size: 14px;
+        }
+
+        p {
+            margin: 0;
+            color: #999999;
+            font-size: 14px;
         }
     }
 </style>
